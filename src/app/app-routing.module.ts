aab5b64a72e753d8ea8ce865f88b9abfb428450e
vishal <[200~vishaltandle800@gmail.comgit config --global user.email vishaltandle800@gmail.com git config --global user.name [200~Vishaltandale987>
2023-08-10 13:52:00 +0530

    form components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { NewFormComponent } from './new-form/new-form.component';
 
-const routes: Routes = [{path: 'todo', component: FormComponent },{path: 'form', component: NewFormComponent }];
+const routes: Routes = [{path: '', component: FormComponent },{path: 'todo', component: FormComponent },{path: 'form', component: NewFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/new-form/new-form.component.ts
+++ src/app/new-form/new-form.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class NewFormComponent {
 
   submit_new_Form(item: any){
-console.log(item)
+    console.log(item.range)
   }
 }
